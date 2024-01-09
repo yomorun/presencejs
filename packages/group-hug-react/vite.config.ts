d1974fffc2050e16234d0f2ccb6224b8e48a51fd
Terry Fei <i@terryfei.com>
2024-01-09 21:20:53 +0800

    feat: use vite-plugin-dts to generate type, and make test demo work

--- packages/group-hug-react/vite.config.ts
+++ packages/group-hug-react/vite.config.ts
@@ -1,11 +1,12 @@
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [react(), dts({ rollupTypes: true })],
   resolve: { alias: { '@': resolve(__dirname, './src') } },
   build: {
     lib: {
@@ -17,7 +18,7 @@ export default defineConfig({
     },
     sourcemap: true,
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', '@yomo/presence'],
     },
   },
 });
